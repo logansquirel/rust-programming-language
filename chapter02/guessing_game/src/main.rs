f16ee6fc2477ea895e45897cb80702ec275a988d
Logan Squirel <logan.squirel@protonmail.com>
2020-06-09 15:12:43 +0200

    Add chapter02/guessing_game source code

--- chapter02/guessing_game/src/main.rs
+++ chapter02/guessing_game/src/main.rs
@@ -1,3 +1,28 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    //println!("The secret number is: {}", secret_number);
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue 
+        };
+        println!("You guessed: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Equal => {
+                println!("You win!");
+                break
+            },
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too big!"),
+        }
+    }
 }
