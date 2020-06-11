5c181d536c59c656dfece7cae3c8760e9df80b6a
Logan Squirel <logan.squirel@protonmail.com>
2020-06-11 10:19:55 +0200

    Update chapter03/booleans/main.rs

--- chapter03/booleans/src/main.rs
+++ chapter03/booleans/src/main.rs
@@ -1,3 +1,41 @@
+#[allow(clippy::nonminimal_bool)]
+#[allow(clippy::eq_op)]
 fn main() {
-    println!("Hello, world!");
+    println!("booleans:");
+    eval!(true);
+    eval!(false);
+    println!("AND:");
+    eval!(true & true);
+    eval!(true & false);
+    eval!(false & true);
+    eval!(false & false);
+    println!("OR:");
+    eval!(true | true);
+    eval!(true | false);
+    eval!(false | true);
+    eval!(false | false);
+    println!("XOR:");
+    eval!(true ^ true);
+    eval!(true ^ false);
+    eval!(false ^ true);
+    eval!(false ^ false);
+    println!("NOT:");
+    eval!(!true);
+    eval!(!false);
+}
+
+#[macro_export]
+macro_rules! stringify {
+    ($x:expr) => {
+        std::stringify!($x)
+    };
+}
+
+#[macro_export]
+macro_rules! eval {
+    ($x:expr) => {
+        let string = stringify!($x);
+        let value = $x;
+        println!("{} = {:?}", string, value);
+    };
 }
