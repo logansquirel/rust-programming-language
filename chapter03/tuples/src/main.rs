9a73f30b30bd2a233318682220f44aab6e26c6a2
Logan SQUIREL <logan.squirel@protonmail.com>
2020-10-22 08:31:50 +0200

    Refactor chapter03/tuples

--- chapter03/tuples/src/main.rs
+++ chapter03/tuples/src/main.rs
@@ -1,14 +1,26 @@
 fn main() {
     // create a tuple
+    println!("initialize tuple");
     let tup: (i32, f64, &str) = (42, 4.2, "42!");
-    println!("{:?}", tup);
+    println!("tup = {:?}", tup);
     // destructuring a tuple
+    println!("destructure tuple");
     let (x, y, z) = tup;
-    println!("{}", x);
-    println!("{}", y);
-    println!("{}", z);
+    println!("x = {}", x);
+    println!("y = {}", y);
+    println!("z = {}", z);
     // accessing tuple elements
-    println!("{}", tup.0);
-    println!("{}", tup.1);
-    println!("{}", tup.2);
+    println!("accessing tuple elements");
+    eval!(tup.0);
+    eval!(tup.1);
+    eval!(tup.2);
+}
+
+#[macro_export]
+macro_rules! eval {
+    ($x:expr) => {
+        let string = stringify!($x);
+        let value = $x;
+        println!("{} = {:?}", string, value);
+    };
 }
